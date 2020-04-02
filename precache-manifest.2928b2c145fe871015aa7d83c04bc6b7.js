5fad2983461b966165cbcfe898aaa7e1453f6d1c
DangThanhTrung1999 <pumaover@gmail.com>
2020-04-02 18:04:46 +0700

    Updates

---
+++ precache-manifest.2928b2c145fe871015aa7d83c04bc6b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11412970717b4967c2aa1307717778d",
+    "url": "/AppNews/index.html"
+  },
+  {
+    "revision": "a2f25b5ee8d83e53c247",
+    "url": "/AppNews/static/css/main.610b5905.chunk.css"
+  },
+  {
+    "revision": "2a39aa9bfe174af98dd5",
+    "url": "/AppNews/static/js/2.01035b72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AppNews/static/js/2.01035b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f25b5ee8d83e53c247",
+    "url": "/AppNews/static/js/main.68fbb619.chunk.js"
+  },
+  {
+    "revision": "9287c7cdcfbfcef980f4",
+    "url": "/AppNews/static/js/runtime-main.1333743c.js"
+  }
+]);
